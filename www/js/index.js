6db0184d40a2a96b526a49237eab45af802cab3c
parest <pares.thibault@outlook.com>
2021-12-05 17:43:33 +0100

    html

--- www/js/index.js
+++ www/js/index.js
@@ -1,3 +1,4 @@
+// initialisation de l'app
 document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
@@ -5,28 +6,7 @@ function onDeviceReady() {
     document.getElementById('deviceready').classList.add('ready');
 }
 
-
-//navigator.geolocation.getCurrentPosition(geolocationSuccess, [geolocationError], [geolocationOptions]);
-/*
-var onSuccess = function(position) {
-    alert('Latitude: '          + position.coords.latitude          + '\n' +
-          'Longitude: '         + position.coords.longitude         + '\n' +
-          'Altitude: '          + position.coords.altitude          + '\n' +
-          'Accuracy: '          + position.coords.accuracy          + '\n' +
-          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-          'Heading: '           + position.coords.heading           + '\n' +
-          'Speed: '             + position.coords.speed             + '\n' +
-          'Timestamp: '         + position.timestamp                + '\n');
-};*/
-
-// onError Callback receives a PositionError object
-/*
-function onError(error) {
-    alert('code: '    + error.code    + '\n' + 'message: ' + error.message + '\n');
-}*/
-
-//navigator.geolocation.getCurrentPosition(onSuccess, onError);
-
+// Début du code pour le plugin geolocation
 
 function getWeatherLocation() {
     navigator.geolocation.getCurrentPosition(
@@ -38,18 +18,8 @@ function getWeatherLocation() {
     );
 }
 
-/*
-// Success callback for get geo coordinates
-var onWeatherSuccess = function (position) {
-    Latitude = position.coords.latitude;
-    Longitude = position.coords.longitude;
-    getWeather(Latitude, Longitude);
-};*/
-
-
 // Get weather by using coordinates
 function getWeather(latitude, longitude) {
-    // Get a free key at http://openweathermap.org/. Replace the "Your_Key_Here" string with that key.
     var OpenWeatherAppKey = "862214dafbb83dc2e46db490e7bc9efb";
     var queryString = 'http://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude + '&appid=' + OpenWeatherAppKey + '&units=imperial';
     $.getJSON(queryString, function (results) {
@@ -81,11 +51,10 @@ function onWeatherError(error) {
     console.log('code: ' + error.code + '\n' + 'message: ' + error.message + '\n');
 }
 
+// Début du code pour le plugin battery-status
 
-/*
-var btn = document.getElementById('get-weather');
-btn.addEventListener('click', function() {
-    getWeatherLocation();
-    console.log("et z'estbarti")
-})
-*/
+
+
+// Début du code pour le plugin camera
+
+// Début du code pour le plugin file
